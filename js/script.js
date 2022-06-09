3fcafd21985d8599e85d3d5d2d3f3e10c52ce3de
mrkekis <zxcurvs@gmail.com>
2022-06-09 12:40:50 +0300

    clock

--- js/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    window.setInterval(function () {
-        var now = new Date();
-        var clock = document.getElementById("clock");
-        clock.innerHTML = now.toLocaleDateString();
-    }, 1000);
-};
